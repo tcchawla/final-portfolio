8f5e81ce2effccc9e768b5d2daa3f41a2b0094e3
tcchawla <tc.chawla2000@gmail.com>
2023-03-27 21:34:20 -0400

    Fixed bug
    Signed-off-by: tcchawla <tc.chawla2000@gmail.com>

--- src/components/canvas/Earth.js
+++ src/components/canvas/Earth.js
@@ -27,7 +27,7 @@ const EarthCanvas = () => {
         fov:45,
         near:0.1,
         far:200,
-        position: [-4, 3, 6]
+        position: [-4, 3, 0]
        }}
     >
       <Suspense fallback={<CanvasLoader />}>
